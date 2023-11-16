bf9a810c35dc4c06229545896f74e7fbb71a6862
Vova Alexyuk <bobaalexs00@gmail.com>
2023-11-16 23:04:22 +0200

    controlled input developed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,10 +4,22 @@ import ButtonGradient from "../../components/ButtonGradient/ButtonGradient";
 import account from "../../assets/svg/big-account.svg";
 import { Link } from "react-router-dom";
 import heart from "../../assets/svg/heart.svg";
+import { useState } from "react";
 
 const Login = () => {
+  const [data, setData] = useState({ username: "", password: "", remember: false });
+
   function handleFormSubmit(event) {
     event.preventDefault();
+    console.log(data);
+  }
+
+  function handleInputChange(e, name) {
+    setData({ ...data, [name]: e.target.value });
+  }
+
+  function handleCheckboxChange(e, name) {
+    setData({ ...data, [name]: e.target.checked });
   }
 
   return (
@@ -19,10 +31,34 @@ const Login = () => {
           </div>
           <h2 className={styles.title}>Увійти</h2>
           <form className={styles.formContainer} onSubmit={handleFormSubmit}>
-            <input type="text" placeholder="Логін" className={styles.input} />
-            <input type="password" placeholder="Пароль" className={styles.input} />
+            <input
+              type="text"
+              placeholder="Логін"
+              className={styles.input}
+              value={data.username}
+              onChange={(e) => {
+                handleInputChange(e, "username");
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Пароль"
+              className={styles.input}
+              value={data.password}
+              onChange={(e) => {
+                handleInputChange(e, "password");
+              }}
+            />
             <label className={styles.checkboxLabel}>
-              <input type="checkbox" className={styles.checkbox} />
+              <input
+                type="checkbox"
+                className={styles.checkbox}
+                value={data.remember}
+                onChange={(e) => {
+                  handleCheckboxChange(e, "remember");
+                  console.log(e);
+                }}
+              />
               Запам'ятати мене
             </label>
             <ButtonGradient
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -4,10 +4,18 @@ import ButtonGradient from "../../components/ButtonGradient/ButtonGradient";
 import account from "../../assets/svg/big-account.svg";
 import { Link } from "react-router-dom";
 import heart from "../../assets/svg/heart.svg";
+import { useState } from "react";
 
 const Registration = () => {
+  const [data, setData] = useState({ username: "", password: "", repeatPassword: "" });
+
   function handleFormSubmit(event) {
     event.preventDefault();
+    console.log(data);
+  }
+
+  function handleInputChange(e, name) {
+    setData({ ...data, [name]: e.target.value });
   }
 
   return (
@@ -19,9 +27,33 @@ const Registration = () => {
           </div>
           <h2 className={styles.title}>Створити акаунт</h2>
           <form className={styles.formContainer} onSubmit={handleFormSubmit}>
-            <input type="text" placeholder="Логін" className={styles.input} />
-            <input type="password" placeholder="Пароль" className={styles.input} />
-            <input type="password" placeholder="Повторіть пароль" className={styles.input} />
+            <input
+              type="text"
+              placeholder="Логін"
+              className={styles.input}
+              value={data.username}
+              onChange={(e) => {
+                handleInputChange(e, "username");
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Пароль"
+              className={styles.input}
+              value={data.password}
+              onChange={(e) => {
+                handleInputChange(e, "password");
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Повторіть пароль"
+              className={styles.input}
+              value={data.repeatPassword}
+              onChange={(e) => {
+                handleInputChange(e, "repeatPassword");
+              }}
+            />
             <ButtonGradient
               type="submit"
               img={heart}
