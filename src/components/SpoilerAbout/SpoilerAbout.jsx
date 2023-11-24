1a4a7a0f77d0e3dc5496f74f59134d2763df776f
Vova Alexyuk <bobaalexs00@gmail.com>
2023-11-24 12:27:56 +0200

    fees page developed

---
+++ src/components/SpoilerAbout/SpoilerAbout.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import styles from "./SpoilerAbout.module.scss";
+import arrow from "../../assets/svg/arrow.svg";
+
+const SpoilerAbout = ({ children, title }) => {
+  const [isVisible, setVisible] = useState(false);
+
+  // console.log(isVisible);
+
+  return (
+    <div className={styles.spoiler}>
+      <h3 className={styles.spoilerTitle} onClick={() => setVisible((param) => !param)}>
+        {title}
+        <motion.img
+          src={arrow}
+          style={{ width: "24px" }}
+          animate={isVisible ? "open" : "closed"}
+          variants={{
+            open: { rotate: 540, transition: { ease: [0.2, 0.7, 0.3, 1], duration: 1 } },
+            closed: { rotate: 0, transition: { ease: [0.2, 0.7, 0.3, 1], duration: 1 } },
+          }}
+        />
+      </h3>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            className={styles.textContainer}
+            animate={{
+              height: "auto",
+              transition: {
+                type: "spring",
+                bounce: 0.3,
+                duration: 1,
+              },
+            }}
+            initial={{ height: 0 }}
+            exit={{
+              height: 0,
+              transition: {
+                duration: 0.5,
+              },
+            }}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default SpoilerAbout;
