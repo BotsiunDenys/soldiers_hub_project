8af133b51edc4b20c66d289dcfe76e200068919f
Denys Botsiun <dbotsyun@gmail.com>
2023-11-29 13:40:00 +0200

    added logic for creating deleting and receiving fees

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
+import App from "./App.jsx";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
