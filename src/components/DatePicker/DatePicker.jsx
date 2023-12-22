caa46949ff891065a758b69ab8f906513eddf25c
Vova Alexyuk <bobaalexs00@gmail.com>
2023-12-22 18:48:53 +0200

    fee create form validation - done, datepicker - updated, using react-date-picker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,58 +1,18 @@
-import { useEffect, useState } from "react";
-import Datepicker from "react-datepicker";
-import styles from "../../pages/FeeCreate/FeeCreate.module.scss";
-import "react-datepicker/dist/react-datepicker.css";
+import Datepicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "./DatePicker.scss";
 
-const DatePicker = ({ value, onChange }) => {
-  // const [date, setDate] = useState(new Date());
-
-  // useEffect(() => dateHandle(date));
-
-  const handleDateChange = (dateValue) => {
-    if (!dateValue) {
-      dateValue = new Date();
-    }
-    const year = String(dateValue.getFullYear());
-    const month = String(dateValue.getMonth() + 1).padStart(2, "0");
-    const day = String(dateValue.getDate()).padStart(2, "0");
-    // onChange(`${day}/${month}/${year}`);
-    console.log(`${day}/${month}/${year}`);
-    console.log(dateValue);
-    return `${day}/${month}/${year}`;
-  };
-
+const DatePicker = ({ value, onChange, onBlur, isValid }) => {
   return (
     <Datepicker
-      // onChange={(e) => {
-      //   // console.log(typeof handleDateChange(e));
-      //   onChange(handleDateChange(e));
-      //   console.log(value);
-      // }}
-      onChange={(e) => onChange(e)}
-      selected={value}
-      className={styles.datePicker}
-      shouldCloseOnSelect={true}
-      // dateFormat="dd/MM/yyyy"
-      // id="datePicker"
+      onChange={onChange}
+      value={value}
+      onBlur={onBlur}
+      className={`${isValid && "isValid"}`}
+      // returnValue="start"
     />
   );
 };
 
 export default DatePicker;
-
-// const DatePicker = ({ dateHandle }) => {
-//   const [date, setDate] = useState(new Date());
-
-//   useEffect(() => dateHandle(date));
-
-//   return (
-//     <Datepicker
-//       onChange={(date) => setDate(date)}
-//       className={styles.datePicker}
-//       shouldCloseOnSelect={true}
-//       selected={date}
-//       dateFormat="dd/MM/yyyy"
-//       id="datePicker"
-//     />
-//   );
-// };
--- src/pages/FeeCreate/FeeCreate.jsx
+++ src/pages/FeeCreate/FeeCreate.jsx
@@ -27,7 +27,7 @@ const FeeCreate = () => {
       backgroundColor: "transparent",
       padding: "5px",
       fontSize: "20px",
-      border: errors?.feeType?.message
+      border: errors?.type?.message
         ? "2px solid #bf1643"
         : menuIsOpen
         ? "2px solid hsl(0, 0%, 70%)"
@@ -53,71 +53,23 @@ const FeeCreate = () => {
       backgroundColor: isFocused ? "hsl(0, 0%, 85%)" : isSelected && "hsl(0, 0%, 70%)",
     }),
   };
-  // const [data, setData] = useState({
-  //   mail: "",
-  //   feeType: "",
-  //   feeName: "",
-  //   feeDescription: "",
-  //   requiredAmount: 0,
-  //   credentials: "",
-  //   image: "",
-  // });
-  const [date, setDate] = useState(new Date());
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const loading = useSelector((state) => state.fees.loading);
   const error = useSelector((state) => state.fees.error);
-  const [validationError, setValidationError] = useState(false);
   const isLogged = useSelector((state) => state.auth.isLogged);
   const success = useSelector((state) => state.fees.creationSuccess);
-  const handleFormSubmit = (data) => {
-    console.log(data);
-    // if (isLogged) {
-    //   if (
-    //     validateEmailRegex.test(data.mail) &&
-    //     data.feeType &&
-    //     data.feeName &&
-    //     data.feeDescription.length <= 300 &&
-    //     data.feeDescription &&
-    //     data.requiredAmount &&
-    //     data.credentials &&
-    //     date !== new Date()
-    //   ) {
-    //     const application = {
-    //       email: data.mail,
-    //       type: data.feeType,
-    //       name: data.feeName,
-    //       description: data.feeDescription,
-    //       sum: data.requiredAmount,
-    //       requisite: data.credentials,
-    //       finish: date,
-    //       isAccepted: false,
-    //       image: data.image,
-    //     };
-    //     dispatch(createApplication(application));
-    //     setValidationError(false);
-    //     setData({
-    //       mail: "",
-    //       feeType: "",
-    //       feeName: "",
-    //       feeDescription: "",
-    //       requiredAmount: 0,
-    //       credentials: "",
-    //       image: "",
-    //     });
-    //   } else {
-    //     setValidationError(true);
-    //   }
-    // } else {
-    //   navigate("/login");
-    // }
-  };
 
-  const handleDateChange = (dateValue) => {
-    const year = String(dateValue.getFullYear());
-    const month = String(dateValue.getMonth() + 1).padStart(2, "0");
-    const day = String(dateValue.getDate()).padStart(2, "0");
-    setDate(`${day}/${month}/${year}`);
+  const handleFormSubmit = (data) => {
+    if (isLogged) {
+      const application = { ...data, date: Date.parse(data.date), isAccepted: false };
+      dispatch(createApplication(application));
+      reset();
+    } else {
+      navigate("/login");
+    }
+    reset();
   };
 
   const {
@@ -126,9 +78,10 @@ const FeeCreate = () => {
     handleSubmit,
     reset,
     control,
-  } = useForm({ mode: "onBlur", defaultValues: { feeName: "", feeType: "" } });
-
-  // console.log("asd");
+  } = useForm({
+    mode: "onBlur",
+    reValidateMode: "onBlur",
+  });
 
   return (
     <main className={styles.main}>
@@ -152,8 +105,8 @@ const FeeCreate = () => {
                 <input
                   type="email"
                   placeholder="Введіть e-mail"
-                  className={`${styles.input} ${errors?.mail && styles.inputError}`}
-                  {...register("mail", {
+                  className={`${styles.input} ${errors?.email && styles.inputError}`}
+                  {...register("email", {
                     required: "Поле обов'язкове до заповнення",
                     minLength: { value: 8, message: "E-mail повинен бути більше 8 символів" },
                     pattern: {
@@ -163,14 +116,15 @@ const FeeCreate = () => {
                   })}
                 />
               </label>
-              <InputErrorMessage>{errors?.mail?.message}</InputErrorMessage>
+              {console.log(errors)}
+              <InputErrorMessage>{errors?.email?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
                 Тип збору
                 <Controller
                   control={control}
-                  name="feeType"
+                  name="type"
                   rules={{ required: "Виберіть тип збору" }}
                   render={({ field: { name, ref, onChange, onBlur, value } }) => (
                     <>
@@ -178,7 +132,7 @@ const FeeCreate = () => {
                         ref={ref}
                         name={name}
                         options={options}
-                        value={options.find((el) => el.value === value)}
+                        value={value && options.find((el) => el.value === value)}
                         onBlur={onBlur}
                         onChange={(el) => {
                           onChange(el.value);
@@ -190,7 +144,7 @@ const FeeCreate = () => {
                   )}
                 />
               </label>
-              <InputErrorMessage>{errors?.feeType?.message}</InputErrorMessage>
+              <InputErrorMessage>{errors?.type?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
@@ -198,22 +152,22 @@ const FeeCreate = () => {
                 <input
                   type="text"
                   placeholder="Введіть назву збору"
-                  className={`${styles.input} ${errors?.feeName && styles.inputError}`}
-                  {...register("feeName", { required: "Поле обов'язкове до заповнення" })}
+                  className={`${styles.input} ${errors?.name && styles.inputError}`}
+                  {...register("name", { required: "Поле обов'язкове до заповнення" })}
                 />
               </label>
-              <InputErrorMessage>{errors?.feeName?.message}</InputErrorMessage>
+              <InputErrorMessage>{errors?.name?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
                 Опис збору до 300 символів
                 <textarea
                   placeholder="Введіть опис збору"
-                  className={`${styles.input} ${errors?.feeDescription && styles.inputError}`}
-                  {...register("feeDescription", { required: "Поле обов'язкове до заповнення" })}
+                  className={`${styles.input} ${errors?.description && styles.inputError}`}
+                  {...register("description", { required: "Поле обов'язкове до заповнення" })}
                 />
               </label>
-              <InputErrorMessage>{errors?.feeDescription?.message}</InputErrorMessage>
+              <InputErrorMessage>{errors?.description?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
@@ -221,8 +175,8 @@ const FeeCreate = () => {
                 <input
                   type="number"
                   placeholder="Введіть суму збору(ціле число)"
-                  className={`${styles.input} ${errors?.requiredAmount && styles.inputError}`}
-                  {...register("requiredAmount", {
+                  className={`${styles.input} ${errors?.sum && styles.inputError}`}
+                  {...register("sum", {
                     required: "Поле обов'язкове до заповнення",
                     pattern: {
                       value: /^[0-9]+$/,
@@ -231,7 +185,7 @@ const FeeCreate = () => {
                   })}
                 />
               </label>
-              <InputErrorMessage>{errors?.requiredAmount?.message}</InputErrorMessage>
+              <InputErrorMessage>{errors?.sum?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
@@ -239,24 +193,30 @@ const FeeCreate = () => {
                 <input
                   type="text"
                   placeholder="Введіть номер банківської картки"
-                  className={`${styles.input} ${errors?.credentials && styles.inputError}`}
-                  {...register("credentials", { required: "Поле обов'язкове до заповнення" })}
+                  className={`${styles.input} ${errors?.requisite && styles.inputError}`}
+                  {...register("requisite", { required: "Поле обов'язкове до заповнення" })}
                 />
               </label>
-              <InputErrorMessage>{errors?.credentials?.message}</InputErrorMessage>
+              <InputErrorMessage>{errors?.requisite?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
                 Кінець збору
                 <Controller
                   control={control}
-                  name="date"
-                  render={({ field: { value, onChange } }) => (
-                    <DatePicker onChange={onChange} value={value} />
+                  name="finish"
+                  rules={{ required: "Вкажіть дату закінчення збору" }}
+                  render={({ field: { value, onChange, onBlur } }) => (
+                    <DatePicker
+                      onChange={onChange}
+                      value={value}
+                      onBlur={onBlur}
+                      isValid={!!errors?.finish}
+                    />
                   )}
                 />
-                {/* <DatePicker dateHandle={handleDateChange} /> */}
               </label>
+              <InputErrorMessage>{errors?.finish?.message}</InputErrorMessage>
             </div>
             <div className={styles.formGroup}>
               <label className={styles.label}>
@@ -290,217 +250,3 @@ const FeeCreate = () => {
 };
 
 export default FeeCreate;
-
-// const FeeCreate = () => {
-//   const [data, setData] = useState({
-//     mail: "",
-//     feeType: "",
-//     feeName: "",
-//     feeDescription: "",
-//     requiredAmount: 0,
-//     credentials: "",
-//     image: "",
-//   });
-//   const [date, setDate] = useState(new Date());
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const loading = useSelector((state) => state.fees.loading);
-//   const error = useSelector((state) => state.fees.error);
-//   const [validationError, setValidationError] = useState(false);
-//   const isLogged = useSelector((state) => state.auth.isLogged);
-//   const success = useSelector((state) => state.fees.creationSuccess);
-//   const handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     if (isLogged) {
-//       if (
-//         validateEmailRegex.test(data.mail) &&
-//         data.feeType &&
-//         data.feeName &&
-//         data.feeDescription.length <= 300 &&
-//         data.feeDescription &&
-//         data.requiredAmount &&
-//         data.credentials &&
-//         date !== new Date()
-//       ) {
-//         const application = {
-//           email: data.mail,
-//           type: data.feeType,
-//           name: data.feeName,
-//           description: data.feeDescription,
-//           sum: data.requiredAmount,
-//           requisite: data.credentials,
-//           finish: date,
-//           isAccepted: false,
-//           image: data.image,
-//         };
-//         dispatch(createApplication(application));
-//         setValidationError(false);
-//         setData({
-//           mail: "",
-//           feeType: "",
-//           feeName: "",
-//           feeDescription: "",
-//           requiredAmount: 0,
-//           credentials: "",
-//           image: "",
-//         });
-//       } else {
-//         setValidationError(true);
-//       }
-//     } else {
-//       navigate("/login");
-//     }
-//   };
-
-//   function handleInputChange(e, name) {
-//     setData({ ...data, [name]: e.target.value });
-//   }
-
-//   const handleSelectChange = (selectedOption) => {
-//     setData({ ...data, feeType: selectedOption.label });
-//   };
-
-//   const handleDateChange = (dateValue) => {
-//     const year = String(dateValue.getFullYear());
-//     const month = String(dateValue.getMonth() + 1).padStart(2, "0");
-//     const day = String(dateValue.getDate()).padStart(2, "0");
-//     setDate(`${day}/${month}/${year}`);
-//   };
-
-//   const {
-//     register,
-//     formState: { errors, isValid },
-//     handleSubmit,
-//     reset,
-//   } = useForm({ mode: "onBlur" });
-
-//   return (
-//     <main className={styles.main}>
-//       <Intro>
-//         <h2 className={styles.introTitle}>Форма для створення благодійного збору</h2>
-//         <p className={styles.introSubtitle}>
-//           Після заповнення даної форми, ваш запит розглянуть технічні спеціалісти.Розгляд заяви
-//           триватиме протягом 1-2 днів, після чого буде опублікований у відповідному розділі.
-//         </p>
-//       </Intro>
-//       <section className={styles.createFee}>
-//         <div className={styles.createFeeContainer}>
-//           <form className={styles.formContainer} onSubmit={handleFormSubmit}>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 E-mail
-//                 <input
-//                   type="email"
-//                   placeholder="Введіть e-mail"
-//                   className={styles.input}
-//                   value={data.mail}
-//                   onChange={(e) => {
-//                     handleInputChange(e, "mail");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 Тип збору
-//                 <Select
-//                   options={options}
-//                   value={options.find((option) => option.value === data.feeType)}
-//                   onChange={handleSelectChange}
-//                   styles={selectStyles}
-//                   placeholder="Виберіть тип збору"
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 Назва збору
-//                 <input
-//                   type="text"
-//                   placeholder="Введіть назву збору"
-//                   className={styles.input}
-//                   value={data.feeName}
-//                   onChange={(e) => {
-//                     handleInputChange(e, "feeName");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 Опис збору до 300 символів
-//                 <textarea
-//                   placeholder="Введіть опис збору"
-//                   className={styles.text}
-//                   value={data.feeDescription}
-//                   onChange={(e) => {
-//                     handleInputChange(e, "feeDescription");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 Необхідна сума збору
-//                 <input
-//                   type="number"
-//                   placeholder="Введіть суму збору(ціле число)"
-//                   className={styles.input}
-//                   value={data.requiredAmount}
-//                   onChange={(e) => {
-//                     handleInputChange(e, "requiredAmount");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>
-//                 Реквізити
-//                 <input
-//                   type="text"
-//                   placeholder="Введіть номер банківської картки"
-//                   className={styles.input}
-//                   value={data.credentials}
-//                   onChange={(e) => {
-//                     handleInputChange(e, "credentials");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label className={styles.label}>Кінець збору</label>
-//               <DatePicker dateHandle={handleDateChange} />
-//             </div>
-//             <div className={styles.formGroup}>
-//               <label htmlFor="filePicker" className={styles.label}>
-//                 Фото збору
-//                 <input
-//                   type="text"
-//                   className={styles.input}
-//                   placeholder="Посилання на фото"
-//                   name="filePicker"
-//                   id="filePicker"
-//                   onChange={(e) => {
-//                     handleInputChange(e, "image");
-//                   }}
-//                 />
-//               </label>
-//             </div>
-//             {error && <p className={styles.error}>Сталась помилка. Спробуйте пізніше!</p>}
-//             {validationError && <p className={styles.error}>Заповніть усі поля!</p>}
-//             {success && (
-//               <p className={styles.success}>Вашу заявку на створення збору успішно надіслано!</p>
-//             )}
-//             {loading && <Loader />}
-//             <ButtonGradient
-//               type="submit"
-//               img={img}
-//               text="Створити збір"
-//               view={{ pading: "10px 30px" }}
-//             />
-//           </form>
-//         </div>
-//       </section>
-//     </main>
-//   );
-// };
